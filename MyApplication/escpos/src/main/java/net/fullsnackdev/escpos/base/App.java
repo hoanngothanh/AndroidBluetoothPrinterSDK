f1a9476b9767369953e2f4c1970e44c8b2d9c59c
HoanNgo - MacOS <nthoan@ipay88.com.my>
2018-11-02 10:22:46 +0700

    update

--- MyApplication/escpos/src/main/java/net/fullsnackdev/escpos/base/App.java
+++ MyApplication/escpos/src/main/java/net/fullsnackdev/escpos/base/App.java
@@ -1,6 +1,9 @@
 package net.fullsnackdev.escpos.base;
 
 import android.app.Application;
+import android.content.Context;
+import android.support.multidex.MultiDex;
+import android.support.multidex.MultiDexApplication;
 
 /**
  * app初始化
@@ -8,10 +11,15 @@ import android.app.Application;
  * Created by liugruirong on 2017/8/3.
  */
 
-public class App extends Application {
+public class App extends MultiDexApplication {
     @Override
     public void onCreate() {
         super.onCreate();
         AppInfo.init(getApplicationContext());
+    }
+
+    @Override
+    protected void attachBaseContext(Context base) {
+        MultiDex.install(this);
     }
 }
