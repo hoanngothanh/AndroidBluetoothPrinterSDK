c0a4b37437d9aafd50bdc498b3f07d2f58b00fa3
HoanNgo - MacOS <nthoan@ipay88.com.my>
2018-11-02 10:35:03 +0700

    context

--- MyApplication/escpos/src/main/java/net/fullsnackdev/escpos/base/App.java
+++ MyApplication/escpos/src/main/java/net/fullsnackdev/escpos/base/App.java
@@ -20,6 +20,7 @@ public class App extends MultiDexApplication {
 
     @Override
     protected void attachBaseContext(Context base) {
+        super.attachBaseContext(base);
         MultiDex.install(this);
     }
 }
